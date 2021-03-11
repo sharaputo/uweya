d20034657421043b2332d2c3db52bf6b6b11497a
Fedor Sharaputo <sharaputo@icloud.com>
2021-03-11 21:56:18 +0300

    fix

--- dev/es6/main.js
+++ dev/es6/main.js
@@ -3,27 +3,28 @@ const menuOpenBtn = document.querySelector('#burger_open');
 const menuCloseBtn = document.querySelector('#burger_close');
 const headerMenu = document.querySelector('#navigation');
 const sideMenuBackdrop = document.querySelector('.side-menu__backdrop');
+const targetScroll = document.querySelector('.side-menu__wrapper');
 
 menuOpenBtn.addEventListener('click', function () {
   headerMenu.classList.add('active');
-  bodyScrollLock.disableBodyScroll(headerMenu);
+  bodyScrollLock.disableBodyScroll(targetScroll);
 });
 
 menuCloseBtn.addEventListener('click', function () {
   headerMenu.classList.remove('active');
-  bodyScrollLock.enableBodyScroll(headerMenu);
+  bodyScrollLock.enableBodyScroll(targetScroll);
 });
 
 window.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     headerMenu.classList.remove('active');
-    bodyScrollLock.enableBodyScroll(headerMenu);
+    bodyScrollLock.enableBodyScroll(targetScroll);
   }
 });
 
 sideMenuBackdrop.addEventListener('click', function () {
   headerMenu.classList.remove('active');
-  bodyScrollLock.enableBodyScroll(headerMenu);
+  bodyScrollLock.enableBodyScroll(targetScroll);
 });
 
 // Header transform effect
