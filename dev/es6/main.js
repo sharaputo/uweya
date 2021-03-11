01984a538237d20d6899af08854523067b041c4e
Fedor Sharaputo <sharaputo@icloud.com>
2021-03-11 19:25:19 +0300

    +

--- dev/es6/main.js
+++ dev/es6/main.js
@@ -6,24 +6,24 @@ const sideMenuBackdrop = document.querySelector('.side-menu__backdrop');
 
 menuOpenBtn.addEventListener('click', function () {
   headerMenu.classList.add('active');
-  bodyScrollLock.disableBodyScroll(menuOpenBtn);
+  bodyScrollLock.disableBodyScroll(headerMenu);
 });
 
 menuCloseBtn.addEventListener('click', function () {
   headerMenu.classList.remove('active');
-  bodyScrollLock.enableBodyScroll(menuOpenBtn);
+  bodyScrollLock.enableBodyScroll(headerMenu);
 });
 
 window.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     headerMenu.classList.remove('active');
-    bodyScrollLock.enableBodyScroll(menuOpenBtn);
+    bodyScrollLock.enableBodyScroll(headerMenu);
   }
 });
 
 sideMenuBackdrop.addEventListener('click', function () {
   headerMenu.classList.remove('active');
-  bodyScrollLock.enableBodyScroll(menuOpenBtn);
+  bodyScrollLock.enableBodyScroll(headerMenu);
 });
 
 // Header transform effect
